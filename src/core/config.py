fc39fa0f4168dcc1d4a8fa27156445ad00370ddc
Amogh Shukla <amoghshukla548@gmail.com>
2026-04-15 21:27:09 +0530

    Add configuration settings using Pydantic for environment variables

---
+++ src/core/config.py
@@ -0,0 +1,22 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    DB_NAME : str
+    DB_USER : str
+    DB_PASSWORD : str
+    DB_HOST : str
+    DB_PORT : int
+
+    SECRET_KEY : str
+    ALGORITHM : str
+    ACCESS_TOKEN_EXPIRE_MINUTES : int
+    ACCESS_TOKEN_EXPIRE_HOURS : int
+
+    @property
+    def DATABSE_URL(self):
+        return f"postgresql://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
