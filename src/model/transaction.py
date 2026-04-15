a8547d24c5bd0f39d55719e8550810e377743344
Amogh Shukla <amoghshukla548@gmail.com>
2026-04-16 00:41:02 +0530

    Add Transaction_Class model with enums for transaction type and status

---
+++ src/model/transaction.py
@@ -0,0 +1,27 @@
+import enum
+from uuid import uuid4
+from src.database.base import Base
+from sqlalchemy import Date, Boolean, Column, DateTime, ForeignKey, Integer, String, Enum as SQLAlchemyEnum
+from sqlalchemy.orm import relationship
+from datetime import datetime, UTC, date
+
+class TransactionType(enum.Enum):
+    CREDIT = "CREDIT"
+    DEBIT = "DEBIT"
+
+class TransactionStatus(enum.Enum):
+    COMPLETED = "COMPLETED"
+    IN_PROCESS = "IN_PROCESS"
+    FAILED = "FAILED"
+
+
+class Transaction_Class(Base):
+    __tablename__="Transaction"
+
+    transaction_id = Column(default =lambda : str(uuid4()))
+    transaction_type = Column(SQLAlchemyEnum(TransactionType), nullable=False)
+    transaction_amount = Column(Integer, nullable=False)
+    transaction_date = Column(Date, default= date.today)
+    transaction_time = Column(DateTime, default= datetime.now(UTC))
+    sender_account_id = Column(String, nullable=False)
+    transaction_status = Column(SQLAlchemyEnum(TransactionStatus), nullable=False)
