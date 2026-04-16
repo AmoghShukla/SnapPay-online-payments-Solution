0b261f9c55a494a287bb3919184dc8aeaa2d3940
Amogh Shukla <amoghshukla548@gmail.com>
2026-04-17 02:12:03 +0530

    feat(auth): implement signup functionality in AuthService and update router

--- src/router/auth.py
+++ src/router/auth.py
@@ -2,9 +2,10 @@ from fastapi import APIRouter, Depends
 from requests import Session
 from src.database.session import get_db
 from src.schema.user import UserCreate, UserResponse
+from src.service.auth import AuthService
 
 router = APIRouter('/auth', tags=['Auth'])
 
 @router.post('/signup', response_model=UserResponse)
 def Signup(payload : UserCreate, db :Session =  Depends(get_db)):
-    pass
+    return AuthService.signup(payload, db)
--- src/service/auth.py
+++ src/service/auth.py
@@ -1,10 +1,11 @@
 from src.exceptions.custom_exceptions import Custom_Exception
+from src.repository.user import UserRepository
 
 class AuthService:
 
     @staticmethod
     def signup(payload , db):
         try:
-            return 
+            return UserRepository.create_user(payload, db)
         except Custom_Exception.RepositoryError as e:
             raise Custom_Exception.ServiceError('Error Encountered in Sevice Layer while creating user') from e
